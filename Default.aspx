f7a2b1aedda6cd1d78cc3b1b38ee329aa880517e
Luiz Parente <31785518+luizparente@users.noreply.github.com>
2018-07-30 14:08:58 -0400

    Add files via upload

---
+++ Default.aspx
@@ -0,0 +1,29 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="Exercise_01.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <h1>Login Page</h1>
+
+            <table style="width:25%">
+              <tr>
+                <td>Login: </td>
+                <td><asp:TextBox ID="txtLogin" runat="server" /><br /></td> 
+              </tr>
+              <tr>
+                <td>Password: </td>
+                <td><asp:TextBox ID="txtPassword" runat="server" TextMode="Password" /> <br /><br /></td>
+              </tr>
+            </table>
+
+            <asp:Button ID="btnLogin" runat="server" Text="Login" />
+        </div>
+    </form>
+</body>
+</html>
---
+++ Default.aspx.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+
+namespace Exercise_01
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        private static int attempts = 0;
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            btnLogin.Click += new EventHandler(this.Login);
+        }
+
+        protected void Login(Object sender, EventArgs e)
+        {
+            string inputLogin = txtLogin.Text;
+            string inputPassword = txtPassword.Text;
+
+            using (SqlConnection conn = new SqlConnection())
+            {
+                conn.ConnectionString = @"Server=DESKTOP-V4SFE6N\SQLEXPRESS;" +
+                    "Database=LPMyDB;" +
+                    "Trusted_Connection=true";
+                conn.Open();
+
+                string query = string.Format($"SELECT * FROM Users WHERE username='{inputLogin}' AND password='{inputPassword}'");
+
+                SqlCommand command = new SqlCommand(query, conn);
+                
+                using (SqlDataReader reader = command.ExecuteReader())
+                {
+                    if (reader.Read())
+                    {
+                        Response.Redirect("Success.aspx");
+                    }
+                    else
+                    {
+                        if (attempts < 3)
+                        {
+                            Response.Write("<script> window.alert('Invalid credentials. Please try again.'); </script>");
+                            attempts++;
+                        }
+                        else
+                        {
+                            Response.Redirect("Fail.aspx");
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
